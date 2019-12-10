b451f4d23cf81c0fa6f70935cc27eaa8179bff35
Suraj <furz_sooraz@hotmail.com>
2019-12-11 00:14:10 +0545

    added payment gateway stripe

---
+++ charge.php
@@ -0,0 +1,102 @@
+<?php
+
+
+  require_once('vendor/autoload.php');
+  require_once('config/db.php');
+  require_once('lib/pdo_db.php');
+  require_once('models/Customer.php');
+  require_once('models/Transaction.php');
+
+
+/*
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+\Stripe\Stripe::setApiKey('sk_test_TULiNvClh1j7QkB4OXYF1s0D001mqpGAEU');
+
+// Token is created using Stripe Checkout or Elements!
+// Get the payment token ID submitted by the form:
+$token = $_POST['stripeToken'];
+$charge = \Stripe\Charge::create([
+    'amount' => 999,
+    'currency' => 'usd',
+    'description' => 'Example charge',
+    'source' => $token,
+]);
+
+
+exit;
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+\Stripe\Stripe::setApiKey('sk_test_TULiNvClh1j7QkB4OXYF1s0D001mqpGAEU');
+
+
+ // Sanitize POST Array
+ $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
+
+ $first_name = $POST['first_name'];
+ $last_name = $POST['last_name'];
+ $email = $POST['email'];
+ $token = $POST['stripeToken'];
+
+// Create Customer In Stripe
+$customer = \Stripe\Customer::create(array(
+  "email" => $email,
+  "source" => $token
+));
+
+// Charge Customer
+$charge = \Stripe\Charge::create(array(
+  "amount" => 5000,
+  "currency" => "usd",
+  "description" => "Intro To React Course",
+  "customer" => $customer->id
+));
+
+// Customer Data
+$customerData = [
+  'id' => $charge->customer,
+  'first_name' => $first_name,
+  'last_name' => $last_name,
+  'email' => $email
+];
+
+// Instantiate Customer
+$customer = new Customer();
+
+// Add Customer To DB
+
+$customer->addCustomer($customerData);
+
+
+// Transaction Data
+$transactionData = [
+  'id' => $charge->id,
+  'customer_id' => $charge->customer,
+  'product' => $charge->description,
+  'amount' => $charge->amount,
+  'currency' => $charge->currency,
+  'status' => $charge->status,
+];
+
+// Instantiate Transaction
+$transaction = new Transaction();
+
+// Add Transaction To DB
+$transaction->addTransaction($transactionData);
+
+// Redirect to success
+header('Location: success.php?tid='.$charge->id.'&product='.$charge->description);
